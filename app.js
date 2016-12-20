8110022df57d20921b17545e943199d1b0c24dd8
siddhuth <siddhuth@gmail.com>
2016-12-20 12:50:10 -0500

    updated app.js

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+var session = require('express-session');
+var flash = require('connect-flash');
+var hbs = require('express-handlebars');
 
 var app = express();
 
-// view engine setup
+app.set('view engine', 'handlebars');
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views'}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -18,12 +19,15 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'secret', resave: true,
+    saveUninitialized: false
+}));
 
-app.use('/', index);
-app.use('/users', users);
+require('./routes/index')(app);
+app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
